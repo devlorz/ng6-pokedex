7a7c4fab2a3b3d52c971524f754f17e1589db5a8
Nutti Saelor <leelorz6@gmail.com>
2018-08-19 22:37:38 +0700

    import sharedmodule into pokemon module

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,24 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { CapitalizePipe } from './../shared/capitalize.pipe';
 import { AppMaterialModule } from './../app-material.module';
 import { PokemonRoutingModule } from './pokemon-routing.module';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { PokemonListWrapperComponent } from './containers/pokemon-list-wrapper/pokemon-list-wrapper.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { FormsModule } from '@angular/forms';
 import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, PokemonRoutingModule, AppMaterialModule, FormsModule],
+  imports: [
+    CommonModule,
+    PokemonRoutingModule,
+    AppMaterialModule,
+    FormsModule,
+    SharedModule
+  ],
   declarations: [
     PokemonCardComponent,
     SearchBarComponent,
     PokemonListComponent,
     PokemonListWrapperComponent,
-    PokemonDetailComponent,
-    CapitalizePipe
+    PokemonDetailComponent
   ]
 })
 export class PokemonModule {}
